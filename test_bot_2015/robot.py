bbcbc02ce7f12a4b39756e10db626e79639bcb2c
killerhamster222 <matt.puentes@gmail.com>
2015-01-11 11:51:31 -0500

    Fixed the wait being outside of the while loop, made CANTalons into just Talons

--- test_bot_2015/robot.py
+++ test_bot_2015/robot.py
@@ -7,8 +7,8 @@ class MyRobot(wpilib.SampleRobot):
         self.joystick1 = wpilib.Joystick(0)
         self.joystick2 = wpilib.Joystick(1)
         
-        self.lmotor = wpilib.CANTalon(5)
-        self.rmotor = wpilib.CANTalon(10)
+        self.lmotor = wpilib.Talon(5)
+        self.rmotor = wpilib.Talon(10)
         
         self.robot_drive = wpilib.RobotDrive(self.lmotor,self.rmotor )
         self.robot_drive.setSafetyEnabled(False)  
@@ -17,8 +17,7 @@ class MyRobot(wpilib.SampleRobot):
         print("Entering Teleop")
         while self.isOperatorControl() and self.isEnabled():
             self.robot_drive.tankDrive(self.joystick1, self.joystick2)
-            
-        wpilib.Timer.delay(.01)
+            wpilib.Timer.delay(.01)
         
     def disabled(self):
         '''Called when the robot is in disabled mode'''
