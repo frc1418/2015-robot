82b5da2ed70ee7688d92a99fe514016915735e02
killerhamster222 <matt.puentes@gmail.com>
2015-01-18 14:19:17 -0500

    Added Text file for autonomus ideas

--- tests/distance sensors test/robot.py
+++ tests/distance sensors test/robot.py
@@ -1,9 +1,14 @@
 import wpilib
 from wpilib.smartdashboard import SmartDashboard
+from wpilib.joystick import Joystick
 
 class MyRobot(wpilib.SampleRobot):
     
     def robotInit(self):
+        self.joystick1 = wpilib.Joystick(0)
+        self.joystick2 = wpilib.Joystick(1)
+        self.joystick3 = wpilib.Joystick(2)
+        
         
         self.smalldistance = wpilib.AnalogInput(0)
         self.largedistance = wpilib.AnalogInput(1)
@@ -11,17 +16,21 @@ class MyRobot(wpilib.SampleRobot):
                 
         
     def operatorControl(self):
-        self.myRobot.setSafetyEnabled(True)
+        #self.myRobot.setSafetyEnabled(True)
 
         while self.isOperatorControl() and self.isEnabled():
+            '''
             wpilib.SmartDashboard.putNumber('smallSensor Voltage', self.smalldistance.getVoltage())
             wpilib.SmartDashboard.putNumber('smallSensor Voltage', self.smalldistance2.getVoltage())
             wpilib.SmartDashboard.putNumber('largeSensor Voltage', self.largedistance.getVoltage())
-
+            '''
+            wpilib.SmartDashboard.putNumber('smallSensor Voltage', self.joystick1.getX())
+            wpilib.SmartDashboard.putNumber('smallSensor Voltage', self.joystick2.getX())
+            wpilib.SmartDashboard.putNumber('largeSensor Voltage', self.joystick3.getX())
             
-            self.fixedSmallValue = ((self.smalldistance.getVoltage())/9.042)**0.8605
-            self.fixedSmallValue2 = ((self.smalldistance2.getVoltage())/9.042)**0.8605
-            self.fixedLargeValue = ((self.smalldistance.getVoltage())/22.73)**0.03081          
+            self.fixedSmallValue = ((max(0,self.smalldistance.getVoltage()))/9.042)**0.8605
+            self.fixedSmallValue2 = ((max(0,self.smalldistance2.getVoltage()))/9.042)**0.8605
+            self.fixedLargeValue = ((max(0,self.smalldistance.getVoltage()))/22.73)**0.03081          
             
             
             wpilib.SmartDashboard.putNumber('smallSensor Value in cm', self.fixedSmallValue)
