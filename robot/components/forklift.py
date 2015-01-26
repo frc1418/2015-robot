aa1540449f64657d4c46c6e110baf098a03f7241
Tim Winters <twinters008@gmail.com>
2015-01-25 23:13:59 -0500

    Added forklift back

---
+++ robot/components/forklift.py
@@ -0,0 +1,15 @@
+import wpilib
+
+class Forklift (object):
+    def __init__ (self, liftmotor):
+        self.lift_motor = liftmotor
+
+
+
+        def setLift(self, speed):
+            self.speed = speed
+
+
+
+    def doit(self):
+        self.lift_motor.set(self.speed)
