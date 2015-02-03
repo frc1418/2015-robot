822f635668bab1c97c853b12350c2c0c9187afa3
killerhamster222 <matt.puentes@gmail.com>
2015-02-03 17:08:02 -0500

    Forklift should work
    we still have to configure the P,I, and D

--- robot/components/forklift.py
+++ robot/components/forklift.py
@@ -7,10 +7,10 @@ class Forklift (object):
         self.setpoint = setpoint
     
     def zeroEnc(self):
-        self.zero=CANMotor.getEncPosition()
+        self.zero=self.encoderToInch(CANMotor.getEncPosition())
     
     def doit(self):
-        self.CANMotor.set(self.position)
+        self.CANMotor.set(self.inchToEncoder(self.position))
 
 
 
@@ -19,8 +19,8 @@ class tote_Forklift (Forklift):
         self.CANMotor=CANMotor
         self.CANMotor = CANMotor
         self.setpoint=CANMotor.getEncPosition()
-        #List of positions
         self.position = (self.setpoint*1440)/5.75
+        #List of positions in inches from 0
         Pn1=0
         P0=1
         P1=2
@@ -32,8 +32,12 @@ class tote_Forklift (Forklift):
         self.lim2 = wpilib.DigitalInput(2)
         self.toteCheckTimer = wpilib.Timer()
     
+    def inchToEncoder(self,c):
+        return (c*1440)/5.75   
+    def encoderToInch(self,c):
+        return (c*5.75)/1440 
+    
     def toteCheck(self):
-        
         if(self.lim1.get() and self.lim2.get()):
             if not self.toteCheckTimer.get()>0:
                 self.toteCheckTimer.start()
@@ -52,7 +56,12 @@ class can_Forklift (Forklift):
         self.CANMotor = CANMotor
         self.setpoint=CANMotor.getEncPosition()
         self.position = (self.setpoint*1440)/9.625
+        #List of positions in inches from 0
         P1=0
         P2=1
         P3=2
-        
+    
+    def inchToEncoder(self,c):
+        return (c*1440)/9.625
+    def encoderToInch(self,c):
+        return (c*9.625)/1440
