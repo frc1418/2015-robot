cd4a1727ea65d6003d1ea39ffea6dd1b2dedd984
Dustin Spicuzza <dustin@virtualroadside.com>
2015-02-07 21:52:03 -0500

    Optimize the button object, add documentation
    - Timer.getMsClock() is more efficient than timer.get(), oddly enough

--- robot/common/button.py
+++ robot/common/button.py
@@ -1,16 +1,21 @@
 import wpilib
 
 class Button:
+    '''Useful utility class for debouncing buttons'''
+    
     def __init__(self,joystick,buttonnum):
         self.joystick=joystick
         self.buttonnum=buttonnum
         self.latest = 0
         self.timer = wpilib.Timer()
-        self.timer.start()
+        
     def get(self):
-        now = self.timer.get()
+        '''Returns the value of the button. If the button is held down, then
+        True will only be returned once every 200ms'''
+        
+        now = self.timer.getMsClock()
         if(self.joystick.getRawButton(self.buttonnum)):
-            if (now-self.latest) > 0.025: 
+            if (now-self.latest) > 200: 
                 self.latest = now
                 return True
         return False
