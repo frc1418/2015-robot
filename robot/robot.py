9c758342b589473201cf23202d3fac9ade49b1ca
Dustin Spicuzza <dustin@virtualroadside.com>
2015-02-10 22:07:36 -0500

    Add bang to top of file

--- robot/robot.py
+++ robot/robot.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 import wpilib
 from components import drive, alignment
@@ -8,7 +9,7 @@ from common.button import Button
 from robotpy_ext.autonomous import AutonomousModeSelector
 
 class MyRobot(wpilib.SampleRobot):
-    
+
     def robotInit(self):
         print("Team 1418's 2015 Code")
 
@@ -34,28 +35,28 @@ class MyRobot(wpilib.SampleRobot):
         self.gyro = wpilib.Gyro(0)
         self.tote_forklift = ToteForklift(5, 2)
         self.can_forklift = CanForklift(15, 3)
-        
+
         self.toteLimitL = wpilib.DigitalInput(0) ##Left limit switch
         self.toteLimitR = wpilib.DigitalInput(1) ##Right limit switch
-        
+
         self.next_pos = 1
-        
+
 
         self.drive = drive.Drive(self.robot_drive, 0)
-        
+
 
         self.longDistanceL = SharpIR2Y0A02(1)  # # Robot's left
         self.longDistanceR = SharpIR2Y0A02(3)  # # Robot's right
         self.shortDistanceL = SharpIRGP2Y0A41SK0F(2)  # # Robot's left
         self.shortDistanceR = SharpIRGP2Y0A41SK0F(7)  # # Robot's right
-        
+
         self.leftSensors = CombinedSensor(self.longDistanceL, 19.5, self.shortDistanceL, 6)
         self.rightSensors = CombinedSensor(self.longDistanceR, 19.5, self.shortDistanceR, 5)
 
         self.align = alignment.Alignment(self.leftSensors, self.rightSensors,
                                          self.toteLimitL, self.toteLimitR,
                                          self.tote_forklift, self.drive)
-        
+
         self.components = {
             'tote_Forklift': self.tote_forklift,
             'can_Forklift': self.can_forklift,
@@ -65,7 +66,7 @@ class MyRobot(wpilib.SampleRobot):
 
         self.control_loop_wait_time = 0.025
         self.automodes = AutonomousModeSelector('autonomous', self.components)
-        
+
         # #Defining Buttons##
         self.canUp = Button(self.joystick1,3)
         self.canDown = Button(self.joystick1,2)
@@ -75,10 +76,10 @@ class MyRobot(wpilib.SampleRobot):
         self.toteDown = Button(self.joystick2, 2)
         self.toteTop = Button(self.joystick2, 6)
         self.toteBottom = Button(self.joystick2, 7)
-        
+
         self.reverseDirection = Button(self.joystick1, 1)
         #self.alignTrigger = Button(self.joystick2, 1)
-        
+
 
     def autonomous(self):
         self.automodes.run(self.control_loop_wait_time, self.update)
@@ -89,65 +90,65 @@ class MyRobot(wpilib.SampleRobot):
         # self.tote_forklift.set_manual(0)
 
         self.logger.info("Entering teleop mode")
-        
+
         while self.isOperatorControl() and self.isEnabled():
             self.drive.move(self.joystick1.getY(), self.joystick1.getX(), self.joystick2.getX())
 
             #
             # Can forklift controls
             #
-            
+
 
             if self.joystick1.getRawButton(5):
                 self.can_forklift.set_manual(1)
-                
+
             elif self.joystick1.getRawButton(4):
                 self.can_forklift.set_manual(-1)
-            
+
             elif self.canUp.get():
                 self.can_forklift.raise_forklift()
-                
+
             elif self.canDown.get():
                 self.can_forklift.lower_forklift()
-                
+
             if self.canTop.get():
                 self.can_forklift.set_pos_top()
             elif self.canBottom.get():
                 self.can_forklift.set_pos_bottom()
-            
+
             # #Tote forklift controls##
-            
-             
+
+
             if self.joystick2.getRawButton(5):
                 self.tote_forklift.set_manual(1)
-                
+
             elif self.joystick2.getRawButton(4):
                 self.tote_forklift.set_manual(-1)
-            
+
             elif self.toteUp.get():
                 self.tote_forklift.raise_forklift()
-                
+
             elif self.toteDown.get():
                 self.tote_forklift.lower_forklift()
-                
+
             if self.toteTop.get():
                 self.tote_forklift.set_pos_top()
             elif self.toteBottom.get():
                 self.tote_forklift.set_pos_bottom()
-                
-                
+
+
             # INFRARED DRIVE#
             if self.joystick2.getTrigger():
                 self.align.align()
-            
-          
+
+
             # REVERSE DRIVE#
             if self.reverseDirection.get():
                 self.drive.switch_direction()
-            
+
             if self.can_forklift.motor.isRevLimitSwitchClosed():
                 self.can_forklift.motor.setSensorPosition(0)
-            
+
             self.smartdashbord_update()
             self.update()
             wpilib.Timer.delay(self.control_loop_wait_time)
@@ -157,23 +158,23 @@ class MyRobot(wpilib.SampleRobot):
         wpilib.SmartDashboard.putNumber('shortSensorValueR', self.shortDistanceR.getDistance())
         wpilib.SmartDashboard.putNumber('longSensorValueL', self.longDistanceL.getDistance())
         wpilib.SmartDashboard.putNumber('longSensorValueR', self.longDistanceR.getDistance())
-        
+
         if self.can_forklift.target_position is None:
             wpilib.SmartDashboard.putNumber('Can Target', -1)
-        else:   
+        else:
             wpilib.SmartDashboard.putNumber('Can Target', self.can_forklift.target_position)
         wpilib.SmartDashboard.putDouble('Can Encoder', self.can_forklift.motor.getEncPosition())
-        
+
         if self.tote_forklift.target_position is None:
             wpilib.SmartDashboard.putNumber('Tote Target', -1)
         else:
             wpilib.SmartDashboard.putNumber('Tote Target', self.tote_forklift.target_position)
         wpilib.SmartDashboard.putDouble('Tote Encoder', self.tote_forklift.motor.getEncPosition())
-        
+
         wpilib.SmartDashboard.putBoolean('ToteLimitL', self.toteLimitL.get())
         wpilib.SmartDashboard.putBoolean('ToteLimitR', self.toteLimitR.get())
-                                         
-    
+
+
 
 
 
@@ -183,9 +184,9 @@ class MyRobot(wpilib.SampleRobot):
 
     def disabled(self):
         '''Called when the robot is in disabled mode'''
-        
+
         self.logger.info("Entering disabled mode")
-        
+
         while not self.isEnabled():
             self.smartdashbord_update()
             wpilib.Timer.delay(.01)
