79e89a30a73be099e627c34f285948949c8eac8e
Dustin Spicuzza <dustin@virtualroadside.com>
2015-03-26 23:27:43 -0400

    Fix simple can and tote autonomous modes

--- robot/autonomous/SimpleAutonomous.py
+++ robot/autonomous/SimpleAutonomous.py
@@ -1,51 +1,56 @@
 from robotpy_ext.autonomous import timed_state, StatefulAutonomous
+from common.custom_stateful import SensorStatefulAutonomous
 
 from components.drive import Drive
 from components.forklift import CanForklift, ToteForklift
 from components.alignment import Alignment
+
 class CanAutonomous(StatefulAutonomous):
-    MODE_NAME='Can Auto'
-    DEFAULT=False
+    
+    MODE_NAME='Can Pickup'
     
     can_forklift = CanForklift
     drive = Drive
+    
     def initialize(self):
         pass
-    def on_enable(self):
-        StatefulAutonomous.on_enable(self)
-    def on_iteration(self, tm):
-        super().on_iteration(tm)
         
     @timed_state(duration=2, next_state='drive_forward', first=True)
     def can_lift(self):
         self.can_forklift.set_pos_stack2()
     
-    @timed_state(duration=3)
+    @timed_state(duration=2)
     def drive_forward(self):
-        self.drive.move(1, 0,0,)
+        self.drive.move(-.5, 0, 0)
         
     
 class ToteAutonomous(StatefulAutonomous):
-    MODE_NAME='Tote Auto'
-    DEFAULT=False
     
-    tote_forklift = CanForklift
+    MODE_NAME='Tote Pickup'
+    
+    tote_forklift = ToteForklift
     drive = Drive
+    
     def initialize(self):
         pass
-    def on_enable(self):
-        StatefulAutonomous.on_enable(self)
-    def on_iteration(self, tm):
-        super().on_iteration(tm)
-    @timed_state(duration=3,first=True)   
+        
+    @timed_state(duration=2, next_state='drive_forward', first=True)
+    def tote_lift(self):
+        self.drive.move(-.1, 0, 0)
+        self.tote_forklift.set_pos_stack2()
+    
+    @timed_state(duration=2)
     def drive_forward(self):
-        self.drive.move(1, 0,0,)
+        self.drive.move(-.5, 0, 0)
+
 class DualAutonomous(StatefulAutonomous):
+    
     MODE_NAME = 'Dual Auto'
-    DEFAULT= False
+    DISABLED = True # let's not bother with this
     
     tote_forklift = ToteForklift  
     can_forklift = CanForklift
+    
     drive = Drive
     align = Alignment
     
