71771efbed031e95f3f21ab94cc5b486a84422e8
Dustin Spicuzza <dustin@virtualroadside.com>
2015-03-27 21:43:20 -0400

    Adjust tote pickup to be more realistic
    - Also, it goes backwards into the auto zone after picking up the totes

--- robot/autonomous/SimpleAutonomous.py
+++ robot/autonomous/SimpleAutonomous.py
@@ -27,26 +27,45 @@ class CanAutonomous(StatefulAutonomous):
 class ToteAutonomous(StatefulAutonomous):
     
     MODE_NAME='Tote Pickup'
+    DEFAULT = False
     
     tote_forklift = ToteForklift
     drive = Drive
     
     def initialize(self):
         pass
+    
+    @timed_state(duration=.5, next_state='get_tote1', first=True)
+    def calibrate(self, initial_call):
         
-    @timed_state(duration=3, next_state='tote_lift2', first=True)
-    def tote_lift(self):
-        self.drive.move(-.5, 0, 0)
+        if initial_call:
+            self.tote_forklift.set_pos_bottom()
+            
+        if self.tote_forklift.isCalibrated:
+            self.next_state('get_tote1')
+        
+    @timed_state(duration=1, next_state='wait_tote1')
+    def get_tote1(self):
+        self.drive.move(-.3, 0, 0)
         self.tote_forklift.set_pos_stack1()
+    
+    @timed_state(duration=2, next_state='goto_tote2')
+    def wait_tote1(self):
+        self.drive.move(0, 0, 0)
         
-    @timed_state(duration=1)
-    def tote_lift2(self):
-        self.drive.move(-.5, 0, 0)
+    @timed_state(duration=1, next_state='get_tote2')
+    def goto_tote2(self):
+        self.drive.move(-.3, 0, 0)
+        
+    @timed_state(duration=1, next_state='backwards')
+    def get_tote2(self):
+        self.drive.move(-.3, 0, 0)
         self.tote_forklift.set_pos_stack2()
     
-    #@timed_state(duration=2)
-    #def drive_forward(self):
-    #    self.drive.move(-.5, 0, 0)
+    @timed_state(duration=5)
+    def backwards(self):
+        '''Get into the autonomous zone'''
+        self.drive.move(.3, 0, 0)
 
 class DualAutonomous(StatefulAutonomous):
     
